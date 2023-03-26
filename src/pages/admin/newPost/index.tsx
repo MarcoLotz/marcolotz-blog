9143d0cc4fcdd4008e4548941442ed1f311f61e5
Pedro Foresti Leão <forestileao@pop-os.localdomain>
2023-03-26 17:39:05 -0300

    feature: added form to new post

--- src/pages/admin/newPost/index.tsx
+++ src/pages/admin/newPost/index.tsx
@@ -1,13 +1,25 @@
 import Editor from '@/components/Editor';
 import { useAuth } from '@/hooks/useAuth';
-import { Button, Container, Flex, Text, TextInput } from '@mantine/core';
+import { Button, Container, Flex, MultiSelect, Text, TextInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
 import { IconDeviceFloppy, IconX } from '@tabler/icons-react';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 
 const NewPost: React.FC = () => {
+  const form = useForm({
+    initialValues: {
+      title: '',
+      category: [],
+      body: ''
+    },
+
+    validate: {
+      title: (value) => !!value ? null : 'Invalid title',
+    },
+  });
+
   const router = useRouter();
-  const [body, setBody] = useState('');
   const { authData } = useAuth();
 
 
@@ -15,17 +27,35 @@ const NewPost: React.FC = () => {
     if (!authData.signedIn)
       router.push('/admin');
   }, []);
+
   return authData.signedIn ? <Container>
-    <Flex mb="2rem" justify="space-between">
-      <Button bg="white" color="red" w="9rem" ml="1rem" variant="outline">
-        <IconX /> <Text ml="0.8rem" size={20}>Cancel</Text>
-      </Button>
-      <Button bg="white" color="green" w="8rem" mr="1rem" variant="outline">
-        <IconDeviceFloppy /> <Text ml="0.8rem" size={20}>Save</Text>
-      </Button>
-    </Flex>
-    <TextInput label="Title" ml="1rem" mr="1rem" mb="2rem" required />
-    <Editor value={body} onChange={value => setBody(value)} />
+    <form onSubmit={form.onSubmit((values) => console.log(values))}>
+      <Flex mb="2rem" justify="space-between">
+        <Button onClick={() => router.push('/')} bg="white" color="red" w="9rem" ml="1rem" variant="outline">
+          <IconX /> <Text ml="0.8rem" size={20}>Cancel</Text>
+        </Button>
+        <Button bg="white" type='submit' color="green" w="8rem" mr="1rem" variant="outline">
+          <IconDeviceFloppy /> <Text ml="0.8rem" size={20}>Save</Text>
+        </Button>
+      </Flex>
+      <TextInput
+        label="Title"
+        ml="1rem"
+        mr="1rem"
+        mb="1rem"
+        required {...form.getInputProps('title')} />
+      <MultiSelect
+        ml="1rem" mr="1rem" mb="2rem"
+        data={[
+          { value: 'Big Data', label: 'Big Data' },
+          { value: 'Embedded Systems', label: 'Embedded Systems' },
+        ]}
+        label="Categories"
+        placeholder="Pick all that you want"
+        {...form.getInputProps('category')}
+      />
+      <Editor {...form.getInputProps('body')} />
+    </form>
   </Container> :
     null
 }
