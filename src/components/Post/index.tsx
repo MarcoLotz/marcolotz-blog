c9490f325c70445e958bfdcc33158e5801a96ad5
Marco Aurelio Lotz <contact@marcolotz.com>
2023-04-05 18:17:57 +0200

    reverted posts index due to css problem

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -2,7 +2,7 @@ import { ActionIcon, Badge, Button, Container, createStyles, Flex, Text, Title, 
 import { IconCalendarTime, IconEdit, IconFolder, IconShare, IconTrash, IconUser } from '@tabler/icons-react';
 import sanitizeHtml from 'sanitize-html';
 import { toast } from 'react-toastify';
-import styles from '/index.module.css'
+
 
 import React, { useCallback } from 'react';
 import Router from 'next/router';
@@ -20,11 +20,51 @@ interface PostData {
   pageIndex?: number; // not static on search
 }
 
+const useStyles = createStyles(() => ({
+  container: {
+    backgroundColor: "white",
+    borderRadius: "0.5rem",
+    padding: "2rem",
+    marginBottom: "1.5rem",
+    border: "0.0625rem solid #ced4da",
+  },
+
+  badgeContainer: {
+    '@media screen and (max-width: 600px)': {
+      flexDirection: 'column'
+    }
+  },
+
+  badge: {
+    '.mantine-Badge-inner': {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      gap: '0.5rem',
+    }
+  },
+  htmlProvider: {
+    'figure, img': {
+      textAlign: 'center',
+      margin: '0 auto',
+      'maxHeight': '500px',
+      display: 'block',
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    },
+    "pre": {
+      backgroundColor: "#e9ebe8"
+    },
+  }
+
+}));
+
 const dateFormatter = new Intl.DateTimeFormat(undefined, {
   dateStyle: 'long'
 });
 
 const Post: React.FC<PostData> = ({ id, title, author, body, category, createdAt, pageIndex }) => {
+  const { classes } = useStyles();
   const { authData } = useAuth();
   const { setEditPost } = useEditPost();
 
@@ -66,32 +106,32 @@ const Post: React.FC<PostData> = ({ id, title, author, body, category, createdAt
     Router.push('/admin/newPost');
   }, [Router, setEditPost]);
 
-  return <Container id={id} className={styles.container}>
+  return <Container id={id} className={classes.container}>
     <Flex justify="space-between">
       <Title mb="1.0rem">{title}</Title>
       {
         authData.signedIn &&
         <Flex gap="0.5rem">
-        <ActionIcon onClick={handleDelete} color="red" variant="filled"><IconTrash /></ActionIcon>
-        <ActionIcon onClick={handleEdit} color="yellow" variant="filled"><IconEdit /></ActionIcon>
-      </Flex>
+          <ActionIcon onClick={handleDelete} color="red" variant="filled"><IconTrash /></ActionIcon>
+          <ActionIcon onClick={handleEdit} color="yellow" variant="filled"><IconEdit /></ActionIcon>
+        </Flex>
       }
     </Flex>
-    <Flex className={styles.badgeContainer} gap='0.5rem' mb="md">
-      <Badge className={styles.badge} color="lime" size="lg" radius="sm" variant="outline">
+    <Flex className={classes.badgeContainer} gap='0.5rem' mb="md">
+      <Badge className={classes.badge} color="lime" size="lg" radius="sm" variant="outline">
         <IconCalendarTime size={16} />
         <Text mt={2.3}>{dateFormatter.format(new Date(createdAt))}</Text>
       </Badge>
-      <Badge className={styles.badge} color="lime" size="lg" radius="sm" variant="outline">
+      <Badge className={classes.badge} color="lime" size="lg" radius="sm" variant="outline">
         <IconUser size={16} />
         <Text mt={2.3}>{author}</Text>
       </Badge>
       {
         category &&
-        <Badge className={styles.badge} color="lime" size="lg" radius="sm" variant="outline">
-        <IconFolder size={16} />
-        <Text mt={2.3}>{category}</Text>
-      </Badge>}
+        <Badge className={classes.badge} color="lime" size="lg" radius="sm" variant="outline">
+          <IconFolder size={16} />
+          <Text mt={2.3}>{category}</Text>
+        </Badge>}
 
       {!!pageIndex && <Button
         onClick={() => copyLink()}
@@ -106,7 +146,7 @@ const Post: React.FC<PostData> = ({ id, title, author, body, category, createdAt
       </Button>}
     </Flex>
 
-    <TypographyStylesProvider className={styles.htmlProvider}>
+    <TypographyStylesProvider className={classes.htmlProvider}>
       <div
         dangerouslySetInnerHTML={{
           __html: sanitizeHtml(body, {
